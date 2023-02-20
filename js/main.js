eedb9cfd32dba8d927bfe5db74429dee24929f25
zai-alvarez <zai.alvarez.l@gmail.com>
2023-02-20 18:37:43 +0100

    estructura

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+let ageDog = document.querySelector('js-ageDog');
+let ageHuman = document.querySelector('js-ageHuman');
+const input = 
+
+if (ageDog === '1') {
+    ageHuman.innerHTML = '7';
+} else {
+    console.log("pepito");
+}
+
